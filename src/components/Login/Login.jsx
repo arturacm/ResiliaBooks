2dacb45ff31c2282c59e9b942ad3e7f3ce3a1e54
Artur Moreira <arturacm@gmail.com>
2021-08-25 22:03:09 -0300

    FEAT: usando eventos no form do Login para buscar a foto do usuario do github

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styled from 'styled-components'
 
+const Formulario = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
 function Login() {
+
+    const [user,setUser] = useState(false)
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(e.target[0].value)
+        // const newUser = require()
+        // console.log(newUser)
+        setUser(`https://github.com/${e.target[0].value}.png`)
+        // try{
+        // }catch(err){
+        //     console.error(err,"algo de errado nao esta certo")
+        // }
+
+    }
+
     return (
         <main>
-            <form action="">
+            <Formulario onSubmit={handleSubmit}>
                 <label htmlFor="usuario">
                     Digite o seu usuario do Github
                 </label>
-                    <input type="text" id="usuario" />
-            </form>
-            
+                <input type="text" id="usuario" />
+                <input  type="submit"/>
+            </Formulario>
+            <img src={user}/>
         </main>
     )
 }
