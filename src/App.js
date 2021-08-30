ed76c178f0fcc849016aa4da6c69368b48421bc8
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-08-30 00:33:24 -0300

    feat: Este commit criará a rota carrinho e removerá o css do component Books

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-//import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Login from './components/Login/Login'
-import {Switch, BrowserRouter, Route} from 'react-router-dom'
-import Books from './components/Books/Books';
-import Book from './components/Book/Book';
+
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+import Books from "./components/Books/Books";
+import Book from "./components/Book/Book";
+import Carrinho from "./components/Carrinho/Carrinho";
 
 function App() {
   return (
@@ -15,34 +16,32 @@ function App() {
         <Switch>
           <Route path="/" exact>
             <main>
-              <h1>Bem vindo {localStorage.getItem('user')} </h1>
-              <Books/>
+              <h1>Bem vindo {localStorage.getItem("user")} </h1>
+              <Books />
             </main>
+          </Route>
+          <Route exact path="/livro/:id">
+            <main>
+              <Book />
+            </main>
+          <Route path="/carrinho/" exact>
+            
+                <Carrinho/>
+
+           
 
           </Route>
-          <Route exact path='/livro/:id'>
-          <main>
-              
-              <Book/>
-            </main>
-           
           </Route>
           <Route path="/login" exact>
-
             <main>
-              <Login/>
+              <Login />
             </main>
-
           </Route>
           <Route path="*">
-
             <main>
               <h1> 404 Not Found</h1>
             </main>
-
           </Route>
-
-
         </Switch>
         <Footer />
       </BrowserRouter>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect,useState} from 'react'
 import {Link} from 'react-router-dom'
-import './Books.css'
+
 function Books() {
     const [livros, setLivros] = useState([])
     useEffect(()=>{
